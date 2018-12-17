af41359ec6a669d775ccd1e6e787d422459aa749
nhht77 <35263094+nhht77@users.noreply.github.com>
2018-12-17 21:40:59 +0200

    Making Carrousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+
 import Header from './components/partials/Header';
+import Feature from './components/Features';
+
+import './resources/styles.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
+        <Feature/>
       </div>
     );
   }
---
+++ src/components/Features.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+import img1 from "../resources/img/img-1.jpg"; 
+import img2 from "../resources/img/img-2.jpg"; 
+import img3 from "../resources/img/img-3.jpg"; 
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+ 
+class DemoCarousel extends Component {
+    render() {
+      const settings = {
+        showThumbs: false,
+        showStatus: false,
+        infiniteLoop: true,
+        autoPlay: true,
+        showIndicators: false
+      }
+
+      const imgSrc = [img1, img2, img3];
+      const img = imgSrc.map( i => {
+        return (
+          <div>
+            <img 
+            src={i}
+            alt="Sungha Jung"
+            style={{height:"100vh"}}/>
+          </div>
+        );
+      })
+        return (
+            <Carousel {...settings}>
+                {img}
+            </Carousel>
+        );
+    }
+};
+ 
+export default DemoCarousel;
